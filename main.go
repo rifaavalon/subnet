e1a12ab36ef1c808806b2ca910ee34c1e3b7e109
Chris Hickey <chrismhickey@gmail.com>
2018-04-17 02:33:36 -0600

    adding changes

--- main.go
+++ main.go
@@ -5,27 +5,38 @@ import (
 	"net"
 )
 
+var err error
+
+func IntToIP(ip uint32) net.IP {
+	result := make(net.IP, 4)
+	result[0] = byte(ip)
+	result[1] = byte(ip >> 8)
+	result[2] = byte(ip >> 16)
+	result[3] = byte(ip >> 24)
+	return result
+}
+
 func main() {
-	var uint32
+	z := IntToIP(0x62D2ED4B).String()
 
 	a := "98.210.237.192/26"
-	b := "98.210.237.193/26"
+	b := z
 
-	ipa, ipneta,_ := net.ParseCIDR(a)
-	ipb, ipnetb,_ := net.ParseCIDR(b)
+	ipa, ipneta, _ := net.ParseCIDR(a)
+	ipb, ipnetb, _ := net.ParseCIDR(b)
 
-	fmt.Println("", a)
-	fmt.Println("", b)
-	fmt.Println("", ipa)
-	fmt.Println("", ipb)
-	fmt.Println("", ipneta)
-	fmt.Println("", ipnetb)
+	fmt.Println("Network address A: ", a)
+	fmt.Println("IP address      B: ", b)
+	fmt.Println("ipA              : ", ipa)
+	fmt.Println("ipnetA           : ", ipneta)
+	fmt.Println("ipB              : ", ipb)
+	fmt.Println("ipnetB           : ", ipnetb)
 
-	fmt.Printf("\nDoes a (%s) contain: b (%s)?\n", ipneta, ipB)
+	fmt.Printf("\nDoes a (%s) contain: b (%s)?\n", ipneta, ipb)
 
-	if ipneta.Contains(ipb){
+	if ipneta.Contains(ipb) {
 		fmt.Println("True")
 	} else {
-	 fmt.Println("False")
+		fmt.Println("False")
 	}
 }
