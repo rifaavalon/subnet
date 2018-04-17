4636e2484c12ce9986e52460696ba6c6fd8a6bc3
Chris Hickey <chrismhickey@gmail.com>
2018-04-17 04:29:18 -0600

    adding changes

--- subnet.rb
+++ subnet.rb
@@ -3,10 +3,10 @@ require 'ipaddr'
 
 net1 = IPAddr.new("98.210.237.192/26")
 p net1.to_i.to_s(16)
-net2 = IPAddr.new("98.210.237.193")
-p net2.to_i.to_s(16)
-net3 = IPAddr.new("98.210.237.75")
-p net3.to_i.to_s(16)
+ip2 = IPAddr.new("98.210.237.193")
+p ip2.to_i.to_s(16)
+ip3 = IPAddr.new("98.210.237.75")
+p ip3.to_i.to_s(16)
 
-p net1.include?(net2)
-p net1.include?(net3)
+p net1.include?(ip2)
+p net1.include?(ip3)
